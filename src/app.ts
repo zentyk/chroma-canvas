1d68af7f2427d09c82174b6d6ab66181d68a0ea6
Zentyk <zentyk@protonmail.com>
2022-11-28 12:10:23 -0600

    # FEAT/E2E + Added Cypress for upcoming testing.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
 import Marrus from "./Engine";
 
 window.onload = function () {
-    let engine = new Marrus.Engine();
-
+    let engine = new Marrus.Engine(document.getElementById('viewport'));
     engine.spriteManager.AddSprites('img.png');
     engine.Init();
 }
