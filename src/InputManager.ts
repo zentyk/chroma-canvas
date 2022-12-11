883c4e2f5b5a013ae583c6ec8475c6c9b71e7a0d
Zentyk <zentyk@protonmail.com>
2022-12-11 15:00:44 -0600

    * removed namespacing

--- src/InputManager.ts
+++ src/InputManager.ts
@@ -1,23 +1,19 @@
-namespace Marrus {
-    class InputManager {
-        constructor() { }
+export default class InputManager {
+    constructor() { }
 
-        OnMouseMove(e) { }
+    OnMouseMove(e) { }
 
-        OnMouseDown(e) { }
+    OnMouseDown(e) { }
 
-        OnMouseUp(e) { }
+    OnMouseUp(e) { }
 
-        OnMouseOut(e) { }
+    OnMouseOut(e) { }
 
-        GetMousePos(canvas, e) {
-            let rect = canvas.getBoundingClientRect();
-            return {
-                x: Math.round((e.clientX - rect.left) / (rect.right - rect.left) * canvas.width),
-                y: Math.round((e.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height)
-            };
-        }
+    GetMousePos(canvas, e) {
+        let rect = canvas.getBoundingClientRect();
+        return {
+            x: Math.round((e.clientX - rect.left) / (rect.right - rect.left) * canvas.width),
+            y: Math.round((e.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height)
+        };
     }
 }
-
-export default Marrus;
--- src/app.ts
+++ src/app.ts
@@ -3,5 +3,6 @@ import Marrus from "./Engine";
 window.onload = function () {
     let engine = new Marrus.Engine(document.getElementById('viewport'));
     engine.spriteManager.AddSprites('img.png');
+
     engine.Init();
 }
