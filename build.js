6cb9683f429c62c5f60db2e6699e204f2890952c
Zentyk <zentyk@protonmail.com>
2022-12-11 18:20:51 -0600

    - testing GH Actions

--- build.js
+++ build.js
@@ -1,18 +1,9 @@
 const esbuild = require('esbuild');
 const { watch } = require('chokidar');
 
-console.log(process.env.NODE_ENV);
+let mode = process.env.NODE_ENV || 'development';
 
-build();
-
-watch('./src/**/*.ts', {})
-    .on('change', (path) => {
-        build();
-    });
-
-function build() {
-    console.log("building...");
-
+function prodBuild() {
     esbuild.build({
         entryPoints: ['./src/app.ts'],
         bundle: true,
@@ -22,9 +13,46 @@ function build() {
         outfile: './dist/app.js',
         tsconfig: './tsconfig.json'
     })
+        .then(() => {
+            console.log("Built!")
+            process.exit(0)
+        })
+        .catch(() => {
+            console.log('Fail during build.')
+            process.exit(1)
+        });
+}
+
+function devBuild() {
+    esbuild.build({
+        entryPoints: ['./src/app.ts'],
+        bundle: true,
+        sourcemap : true,
+        target : "ES2015",
+        minify : false,
+        outfile: './dist/app.js',
+        tsconfig: './tsconfig.json'
+    })
         .then(() => console.log("Built!"))
         .catch(() => {
             console.log('Fail during build.')
             process.exit(1)
         });
 }
+
+if (mode === 'development') {
+    console.log('Development mode');
+    devBuild();
+} else {
+    console.log('Production mode');
+    prodBuild();
+}
+
+watch('./src/**/*.ts', {})
+    .on('change', (path) => {
+        if(mode === 'development') {
+            devBuild();
+        } else {
+            prodBuild();
+        }
+    });
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,5 @@ import Marrus from "./Engine";
 window.onload = function () {
     let engine = new Marrus.Engine(document.getElementById('viewport'));
     engine.spriteManager.AddSprites('img.png');
-
     engine.Init();
 }
