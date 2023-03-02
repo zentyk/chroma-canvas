580db6b6cd813992ec3b952a1666e71a1f23f7de
Zentyk <zentyk@protonmail.com>
2023-03-02 13:57:01 -0600

    * Modified Engine

--- src/Engine.ts
+++ src/Engine.ts
@@ -5,12 +5,56 @@ namespace Marrus {
     export class Engine {
         public canvas : HTMLElement;
         public context : CanvasRenderingContext2D;
-        public lastFrame : number;
-        public fpsTime : number;
-        public frameCount : number;
-        public fps : number;
-        public inputManager : InputManager;
         public spriteManager : SpriteManager;
+        public inputManager : InputManager;
+        private config : any;
+        private then : any;
+        private delta: any;
+        private now: any;
+        constructor(config : any) {
+            this.config = config;
+            this.then = Date.now();
+            this.spriteManager = new SpriteManager();
+            this.spriteManager.AddSprites('img.png');
+            this.Init();
+        }
+        Init() {
+            try {
+                this.canvas = document.getElementById(this.config.canvasId);
+                if(this.canvas instanceof HTMLCanvasElement) {
+                    this.context = this.canvas.getContext(this.config.context);
+                } else {
+                    throw new Error(`${this.config.errorTypes.ENERR}: Canvas element not found`);
+                }
+            } catch(e) {
+                alert(e.message);
+            }
+            this.spriteManager.LoadSprites();
+            //this.canvas.addEventListener('mousemove',this.inputManager.OnMouseMove);
+            //this.canvas.addEventListener('mousedown',this.inputManager.OnMouseDown);
+            //this.canvas.addEventListener('mouseup',this.inputManager.OnMouseUp);
+            //this.canvas.addEventListener('mouseout',this.inputManager.OnMouseOut);
+            this.Main();
+        }
+
+        Main() {
+            this.then = Date.now();
+            this.Render();
+        }
+
+        private Render() {
+            this.now = Date.now();
+            this.delta = this.now - this.then;
+            if(this.delta > 1000/this.config.fps){
+                this.Draw();
+                this.then = this.now;
+            }
+            window.requestAnimationFrame(this.Render.bind(this));
+        }
+
+        private Draw() {
+            this.context.drawImage(this.spriteManager.images[0],0,0);
+        }
     }
 }
 
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,29 @@
 import Marrus from "./Engine";
 
+enum errorTypes {
+    ENERR = "Engine Error",
+    SYSERR = "System Error",
+    GRAPHERR = "Graphics Error",
+    MATHERR = "Math Error",
+    NETERR = "Network Error",
+    GERR = "Game Error",
+    FILEERR = "File Error",
+    MEMERR = "Memory Error",
+    SOUNDERR = "Sound Error",
+}
+
+class Config {
+    private title = "Marrus";
+    private fps = 60;
+    private width = 800;
+    private height = 600;
+    private backgroundColor = '#16725e';
+    private context = "2d";
+    public readonly canvasId = "viewport";
+    public readonly errorTypes = errorTypes;
+}
+
 window.onload = function () {
-    let engine = new Marrus.Engine(document.getElementById('viewport'));
-    engine.spriteManager.AddSprites('img.png');
-    engine.Init();
+    let config = new Config();
+    let engine = new Marrus.Engine(config);
 }
