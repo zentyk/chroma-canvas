6901f91312a5ca2bf85ddf6b5fdfc900a343c5a9
Zentyk <zentyk@protonmail.com>
2023-03-17 13:24:43 -0600

    * Fixed Error in items

--- src/Game.ts
+++ src/Game.ts
@@ -62,18 +62,8 @@ export default class MainGame extends Scene {
     }
 
     CreateLevel() {
-        let done = false;
-
-        //while(!done) {
         this.match3.GenerateBoard();
         this.DrawBoard();
-            //this.ResolveClusters();
-            //this.FindMoves();
-
-            /*if(this.moves.length > 0) {
-                done = true;
-            }*/
-        //}
     }
 
     public ClearLevel() {
@@ -151,11 +141,14 @@ export default class MainGame extends Scene {
                 this.AddObject('selected2', sp2);
                 let canSwap = this.match3.CanSwapItems();
                 if(canSwap) {
-                    this.match3.SwapItems();
+                    this.match3.swap(this.match3.selectedItems[0].column, this.match3.selectedItems[0].row, this.match3.selectedItems[1].column, this.match3.selectedItems[1].row);
                     this.SwapSprites();
                     this.match3.ClearSelectedItems();
                     this.RemoveObject('selected1');
                     this.RemoveObject('selected2');
+                    this.match3.ResolveClusters();
+                    this.match3.FindMoves();
+                    this.DrawBoard();
                 } else {
                     this.RemoveObject('selected1');
                     this.RemoveObject('selected2');
@@ -191,26 +184,41 @@ class Match3 {
     private clusters = [];
     private score = 0;
     public selectedItems = [];
-    private currentMove = {column:0,row:0,direction:0};
+    private currentMove = {column1:0,row1:0,column2:0,row2:0};
     private gameStates = {init:0,ready:1,resolve:2};
     private gameState = this.gameStates.init;
     private gameover = false;
 
     constructor() {
         this.items = [];
+        this.clusters = [];
         this.selectedItems = [];
     }
 
     public GenerateBoard() {
-        for (let i = 0; i < this.columns; i++) {
-            this.items[i] = [];
-            for (let j = 0; j < this.rows; j++) {
-                let type = Math.floor(Math.random() * 6);
-                this.items[i][j] = new Item(i, j, type, false, null);
+        let done = false;
+        while(!done) {
+            for (let i = 0; i < this.columns; i++) {
+                this.items[i] = [];
+                for (let j = 0; j < this.rows; j++) {
+                    let type = this.GetRandomItem();
+                    this.items[i][j] = new Item(i, j, type, false, null);
+                }
+            }
+            this.ResolveClusters();
+            this.FindMoves();
+
+            if(this.moves.length > 0) {
+                done = true;
             }
         }
     }
 
+    private GetRandomItem() {
+        return Math.floor(Math.random() * 6);
+    }
+
+    //#region Item Selection and Swapping
     public GetValidItem(column : number, row : number) {
         if(column < 0 || column >= this.columns || row < 0 || row >= this.rows) {
             return false;
@@ -255,88 +263,224 @@ class Match3 {
         let item2Type = this.items[item2.column][item2.row].type;
         this.items[item1.column][item1.row].type = item2Type;
         this.items[item2.column][item2.row].type = item1Type;
-        let clusters = this.FindClusters();
-        if(clusters.length > 0) {
+        this.FindClusters();
+        if(this.clusters.length > 0) {
             return true;
         }
         return false;
     }
 
-    SwapItems() {
-        let item1 = this.selectedItems[0];
-        let item2 = this.selectedItems[1];
-        let item1Type = this.items[item1.column][item1.row].type;
-        let item2Type = this.items[item2.column][item2.row].type;
-        this.items[item1.column][item1.row].type = item2Type;
-        this.items[item2.column][item2.row].type = item1Type;
-    }
-
-    private FindClusters() {
-        let clusters = [];
-        for(let i = 0; i < this.columns; i++) {
-            for(let j = 0; j < this.rows; j++) {
-                let type = this.items[i][j].type;
-                let horizontalCluster = this.FindHorizontalCluster(i,j,type);
-                if(horizontalCluster.length >= 3) {
-                    clusters.push(horizontalCluster);
-                }
-                let verticalCluster = this.FindVerticalCluster(i,j,type);
-                if(verticalCluster.length >= 3) {
-                    clusters.push(verticalCluster);
-                }
-            }
-        }
-        return clusters;
-    }
-
-    private FindHorizontalCluster(i: number, j: number, type: any) {
-        let cluster = [];
-        let left = i;
-        let right = i;
-        while(left > 0 && this.items[left - 1][j].type === type) {
-            left--;
-        }
-        while(right < this.columns - 1 && this.items[right + 1][j].type === type) {
-            right++;
-        }
-        for(let k = left; k <= right; k++) {
-            cluster.push({column:k,row:j});
-        }
-        return cluster;
-    }
-
-    private FindVerticalCluster(i: number, j: number, type: any) {
-        let cluster = [];
-        let top = j;
-        let bottom = j;
-        while(top > 0 && this.items[i][top - 1].type === type) {
-            top--;
-        }
-        while(bottom < this.rows - 1 && this.items[i][bottom + 1].type === type) {
-            bottom++;
-        }
-        for(let k = top; k <= bottom; k++) {
-            cluster.push({column:i,row:k});
-        }
-        return cluster;
-    }
-
     ClearSelectedItems() {
         this.selectedItems = [];
     }
+
+    private CanMoveItem(i: number, j: number, number: number) {
+        if(number === 0) {
+            if(i > 0 && this.items[i - 1][j].type === this.items[i][j].type) {
+                return true;
+            }
+            if(i < this.columns - 1 && this.items[i + 1][j].type === this.items[i][j].type) {
+                return true;
+            }
+        }
+        if(number === 1) {
+            if(j > 0 && this.items[i][j - 1].type === this.items[i][j].type) {
+                return true;
+            }
+            if(j < this.rows - 1 && this.items[i][j + 1].type === this.items[i][j].type) {
+                return true;
+            }
+        }
+        return false;
+    }
+    //#endregion
+
+    //#region Cluster Shifting
+    public ResolveClusters() {
+        this.FindClusters();
+        while(this.clusters.length > 0) {
+            this.ClearClusters();
+            this.ShiftTiles();
+            this.FindClusters();
+        }
+    }
+
+    private FindClusters() {
+        this.clusters = [];
+
+        // Check for horizontal clusters
+        for(let j = 0; j < this.rows; j++) {
+            let matchLength = 1;
+            for(let i = 0; i < this.columns; i++) {
+                let checkCluster = false;
+                if(i === this.columns-1) {
+                    checkCluster = true;
+                } else {
+                    if(
+                        this.items[i][j].type === this.items[i+1][j].type &&
+                        this.items[i][j].type !== -1) {
+                        matchLength+=1;
+                    } else {
+                        checkCluster = true;
+                    }
+                }
+                if(checkCluster) {
+                    if(matchLength === 3) {
+                        this.clusters.push({
+                            column:i+1-matchLength,
+                            row:j,
+                            length:matchLength,
+                            direction:'h'});
+                    }
+                    if(matchLength === 4){
+                        this.clusters.push({
+                            column:i+1-matchLength,
+                            row:j,
+                            length:matchLength,
+                            direction:'h'});
+                    }
+                    if(matchLength === 5){
+                        this.clusters.push({
+                            column:i+1-matchLength,
+                            row:j,
+                            length:matchLength,
+                            direction:'h'});
+                    }
+                    matchLength = 1;
+                }
+            }
+        }
+        //check for vertical clusters
+        for(let i = 0; i < this.columns; i++) {
+            let matchLength = 1;
+            for(let j = 0; j < this.rows; j++) {
+                let checkCluster = false;
+                if(j === this.rows-1) {
+                    checkCluster = true;
+                } else {
+                    if(this.items[i][j].type === this.items[i][j + 1].type &&
+                        this.items[i][j].type !== -1) {
+                        matchLength+=1;
+                    } else {
+                        checkCluster = true;
+                    }
+                }
+                if(checkCluster) {
+                    if(matchLength === 3) {
+                        this.clusters.push({column:i,row:j+1-matchLength,length:matchLength,direction:'v'});
+                    }
+                    if(matchLength === 4) {
+                        this.clusters.push({column:i,row:j+1-matchLength,length:matchLength,direction:'v'});
+                    }
+                    if(matchLength === 5) {
+                        this.clusters.push({column:i,row:j+1-matchLength,length:matchLength,direction:'v'});
+                    }
+                    matchLength = 1;
+                }
+            }
+        }
+    }
+
+    private ClearClusters() {
+        let that =this;
+        this.loopClusters(function(index,column,row,cluster){
+            that.items[column][row].type = -1;
+        });
+        for(let i = 0; i < this.columns; i++) {
+            let shift = 0;
+            for(let j = this.rows-1; j>=0; j--) {
+                if(this.items[i][j].type === -1) {
+                   shift++;
+                   this.items[i][j].shift = 0;
+                } else {
+                    this.items[i][j].shift = shift;
+                }
+            }
+        }
+    }
+
+    private loopClusters(callback: (index: number, column: number, row: number, cluster: any) => void) {
+        for(let i = 0; i < this.clusters.length; i++) {
+            let cluster = this.clusters[i];
+            let cOffset = 0;
+            let rOffset = 0;
+            for(let j = 0; j < cluster.length; j++) {
+                callback(i,cluster.column + cOffset,cluster.row + rOffset,cluster);
+                if(cluster.direction === 'h') {
+                    cOffset++;
+                } else {
+                    rOffset++;
+                }
+            }
+        }
+    }
+
+    private ShiftTiles() {
+        for(let i = 0; i < this.columns; i++) {
+            for(let j = this.rows - 1; j >= 0; j--) {
+                if(this.items[i][j].type === -1) {
+                    this.items[i][j].type = this.GetRandomItem();
+                } else {
+                    let shift = this.items[i][j].shift;
+                    if(shift > 0) {
+                        this.swap(i,j,i,j+shift);
+                    }
+                }
+                this.items[i][j].shift = 0;
+            }
+        }
+    }
+
+    public swap(x1: number, y1: number, x2: number, y2: any) {
+        let typeSwap = this.items[x1][y1].type;
+        if(x2 < this.columns-1 || y2 > this.rows-1){
+            this.items[x1][y1].type = this.items[x2][y2].type;
+            this.items[x2][y2].type = typeSwap;
+        }
+    }
+
+    public FindMoves() {
+        this.moves = [];
+        for(let i = 0; i < this.rows; i++) {
+            for(let j = 0; j < this.columns-1; j++) {
+                console.log(this.items[i][j]);
+                this.swap(i,j,i+1,j);
+                this.FindClusters();
+                this.swap(i,j,i+1,j);
+                if(this.clusters.length > 0) {
+                    this.moves.push({column1:i,row1:j,column2:i+1,row2:j});
+                }
+            }
+
+        }
+
+        for(let i =0; i<this.columns;i++){
+            for(let j=0;j<this.rows-1;j++){
+                this.swap(i,j,i,j+1);
+                this.FindClusters();
+                this.swap(i,j,i,j+1);
+                if(this.clusters.length > 0) {
+                    this.moves.push({column1:i,row1:j,column2:i,row2:j+1});
+                }
+            }
+        }
+
+       this.clusters = [];
+    }
+    //#endregion
 }
 class Item {
     column : number;
     row : number;
     type : number;
-    empty : boolean;
+    shift : number;
     sprite : Sprite;
 
     constructor(column : number, row : number, type : number, empty : boolean, sprite : Sprite) {
         this.column = column;
         this.row = row;
         this.type = type;
-        this.empty = true;
         this.sprite = sprite;
+        this.shift = 0;
     }
 }
